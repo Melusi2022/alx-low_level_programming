b2af6fd3d9d4f3c9d85c88ef9b66fff6d2a6a832
Melusi2022 <melusimedia@gmail.com>
2023-07-25 12:33:40 -0700

    98 Battery street

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Function that takes a pointer to an int
+ * and updates the value it points to 98
+ * @n: the value to be updated to 98
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}

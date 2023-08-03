658d45d3c1b07766939afbe81302a40386a66ec6
Melusi2022 <melusimedia@gmail.com>
2023-08-03 14:33:15 -0700

    function that returns 1 if the input integer

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * is_prime_recursive - check if a number is prime recursively
+ * @n: input number
+ * @divisor: the current divisor being tested
+ * Return: 1 if prime otherwise 0
+ */
+int is_prime_recursive(int n, int divisor)
+{
+	if (n <= 1)
+		return (0);
+	if (divisor == 1)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+
+	return (is_prime_recursive(n, divisor - 1));
+			}
+
+			/**
+			 * is_prime_number - check if the number is prime
+			 *
+			 * @n: input
+			 *
+			 * Return: 1 if prime, 0 otherwise
+			 */
+			int is_prime_number(int n)
+			{
+			if (n <= 1)
+			return (0);
+			return (is_prime_recursive(n, n - 1));
+			}
